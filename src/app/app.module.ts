7783db5b1708e101a3f675880682714b700a278a
Simón Cebrero <62322984+simoncebrero095@users.noreply.github.com>
2021-03-08 18:06:01 -0300

    agregamos el auth module / movimos la carpeta nopagefound

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,25 @@ import { NgModule } from '@angular/core';
 // Modulos 
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     NopagefoundComponent,
-   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     PagesModule,
+    AuthModule,
 
   ],
   bootstrap: [AppComponent]
